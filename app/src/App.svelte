a5410b75a5588031a4a1a1973e171d8e72da317d
Frank Basham <frankabasham@gmail.com>
2023-06-17 20:04:53 -0400

    [app] add logic to random gen, lock,  and favourites

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -6,12 +6,38 @@
     let res = await fetch("http://127.0.0.1:8000");
     data = (await res.json()).data;
   });
+
+  let numNames = 1;
+  $: names = Array.from({ length: numNames }, () => ({
+    name: "",
+    locked: false,
+  }));
+  let favourites = new Set();
+  function getRandomName() {
+    return data[(Math.random() * data.length) | 0];
+  }
 </script>
 
-{#each data as d}
+<h1>Svlete Names</h1>
+
+<input type="number" min="1" max="5" bind:value={numNames} />
+<button
+  on:click={() =>
+    (names = names.map((o) => (o.locked ? o : { ...o, ...getRandomName() })))}
+  >random</button
+>
+{#each names as o, i}
   <div>
-    {d.name}
-    {d.gender}
-    {d.freq}
+    <p>{o.name ?? "x"}</p>
+    <button on:click={() => (names[i] = { ...names[i], locked: !o.locked })}
+      >{o.locked ? "un" : ""}lock</button
+    >
+    <button on:click={() => (favourites = new Set([...favourites, o]))}
+      >heart</button
+    >
   </div>
+{/each}
+
+{#each [...favourites] as fav}
+  {fav.name}
 {/each}
